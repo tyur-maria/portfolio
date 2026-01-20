11a6030df559631a6626954f977ac3c13e78702c
tyur-maria <m.tyur2005@gmail.com>
2026-01-20 13:08:19 +0300

    Add files via upload

---
+++ project/project/telegram.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_POST['client_name']; // получаем имя клиента
+$email = $_POST['client_email']; // получаем почту клиента
+$message = $_POST['client_message']; // получаем сообщение клиента
+
+// воодим между кавычек токен бота, который прислал @botfater
+$token = "8318011799:AAG67ScCwL7bnTlz4MvR6StkPiKkR9bemTc"; 
+// вставляем номер чата, который можно найти на странице 
+// api.telegram.org/botXXXXXXXXX/getUpdates — где XXX это токен бота
+$chat_id = "758114109";
+
+// Собираем данные в один массив 
+$arr = array(
+  'Клиент: ' => $name,
+  'Email: ' => $email,
+  'Сообщение: ' => $message
+);
+
+// составляем сообщение из данных массива
+foreach($arr as $key => $value) {
+  $txt .= $key."<b> ". urlencode($value)."</b> "."%0A";
+};
+
+// даем команду боту отправить сообщение с текстом
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) { 
+  return true; // если прошло успешно, возвращаем ответ true
+} else {
+  return false; // если ошибка, возвращаем false
+}
+?>
